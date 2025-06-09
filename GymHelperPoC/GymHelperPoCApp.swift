43c77ecbb0f181a721d3dc57f09a45d897b8049f
Andrii Matsevytyi <macevitiya@i.ua>
2025-06-09 13:57:20 +0300

    Initial Commit

---
+++ GymHelperPoC/GymHelperPoCApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymHelperPoCApp.swift
+//  GymHelperPoC
+//
+//  Created by Andrii Matsevytyi on 09.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct GymHelperPoCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
