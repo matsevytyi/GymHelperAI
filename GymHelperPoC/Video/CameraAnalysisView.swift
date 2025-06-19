ea3776afb63d50fef8d27b15712819b8872b5a8c
Andrii Matsevytyi <macevitiya@i.ua>
2025-06-19 15:14:13 +0200

    added camera preview view

--- GymHelperPoC/Video/CameraAnalysisView.swift
+++ GymHelperPoC/Video/CameraAnalysisView.swift
@@ -10,7 +10,18 @@ import SwiftUI
 import AVFoundation
 
 
+struct CameraPreviewView: UIViewRepresentable {
+    let session: AVCaptureSession
 
+    func makeUIView(context: Context) -> VideoPreviewView {
+        let view = VideoPreviewView()
+        view.videoPreviewLayer.session = session
+        view.videoPreviewLayer.videoGravity = .resizeAspectFill
+        return view
+    }
+
+    func updateUIView(_ uiView: VideoPreviewView, context: Context) {}
+}
 
 class VideoPreviewView: UIView {
     override class var layerClass: AnyClass {
